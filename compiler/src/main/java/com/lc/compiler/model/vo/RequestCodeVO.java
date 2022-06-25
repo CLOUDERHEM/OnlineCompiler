a5732a905650b8dd99d9e762a4e81eba924d654d
CLOUDERHEM <cloudac@qq.com>
2022-06-25 16:48:52 +0800

    fix: 添加language num限制

--- compiler/src/main/java/com/lc/compiler/model/vo/RequestCodeVO.java
+++ compiler/src/main/java/com/lc/compiler/model/vo/RequestCodeVO.java
@@ -2,6 +2,8 @@ package com.lc.compiler.model.vo;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -11,6 +13,8 @@ import javax.validation.constraints.NotNull;
  */
 @Data
 public class RequestCodeVO {
+    @Max(value = 4)
+    @Min(value = 1)
     @NotNull(message = "language 不能为空")
     private Integer language;
     private String input;
